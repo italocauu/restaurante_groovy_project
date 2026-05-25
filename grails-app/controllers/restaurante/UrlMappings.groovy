54fa6df2c5f000198977c343f3c782d5ab70bb32
italocauu <ivtcau@gmail.com>
2026-05-25 17:41:24 -0300

    Correção das rotas URL

--- grails-app/controllers/restaurante/UrlMappings.groovy
+++ grails-app/controllers/restaurante/UrlMappings.groovy
@@ -14,10 +14,10 @@ class UrlMappings {
         "404"(view:'/notFound')
 		
         // === SUAS NOVAS ROTAS MANUAIS DA API ===
-        "/api/clientes"(controller: "cliente", action: "index", method: "GET")
-        "/api/clientes"(controller: "cliente", action: "save", method: "POST")
-        "/api/clientes/$id"(controller: "cliente", action: "show", method: "GET")
-        "/api/clientes/$id"(controller: "cliente", action: "update", method: "PUT")
-        "/api/clientes/$id"(controller: "cliente", action: "delete", method: "DELETE")
+        "/api/$Controller"(controller: "cliente", action: "index", method: "GET")
+        "/api/$Controller"(controller: "cliente", action: "save", method: "POST")
+        "/api/$Controller/$id"(controller: "cliente", action: "show", method: "GET")
+        "/api/$Controller/$id"(controller: "cliente", action: "update", method: "PUT")
+        "/api/$Controller/$id"(controller: "cliente", action: "delete", method: "DELETE")
     }
 }
